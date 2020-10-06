cb7edebd4e7e5b85acd73c3e6cb7fce101812331
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-10-06 16:48:15 +0900

    change

---
+++ build/precache-manifest.c36d7a06b31f334ec3dc1ecb18140e71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22391f20e71e9cbf4284c51d5ffeae2",
+    "url": "/fun-fac-web/index.html"
+  },
+  {
+    "revision": "53e3820d04c22ee3ea7c",
+    "url": "/fun-fac-web/static/css/main.135656bf.chunk.css"
+  },
+  {
+    "revision": "1e978999d4e69be4daee",
+    "url": "/fun-fac-web/static/js/2.bbc04556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fun-fac-web/static/js/2.bbc04556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e3820d04c22ee3ea7c",
+    "url": "/fun-fac-web/static/js/main.2fef4eaf.chunk.js"
+  },
+  {
+    "revision": "a1268159c681d5d9fce5",
+    "url": "/fun-fac-web/static/js/runtime-main.27179b96.js"
+  },
+  {
+    "revision": "d88a42538b87d2019d5a33a951bb1147",
+    "url": "/fun-fac-web/static/media/logo.d88a4253.png"
+  },
+  {
+    "revision": "dc9787f4e8bca3f2c5c6d4b53cb0a4d4",
+    "url": "/fun-fac-web/static/media/trigger.dc9787f4.jpg"
+  },
+  {
+    "revision": "9ae8dd5bb50d1e84c4c20533e304f63c",
+    "url": "/fun-fac-web/static/media/trigger2.9ae8dd5b.jpg"
+  }
+]);
