5debbb371a551a1b65564d7c51e4f14fb4a9ee04
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-10-06 16:10:29 +0900

    added scroll to top and reduced image size

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Navigation from "./js/components/Navigation";
 import CluePage from "./js/routes/CluePage";
 import ContentPage from "./js/routes/ContentPage";
+import ScrollToTop from "./js/components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
+      <ScrollToTop />
       <Switch>
         <Route path="/clue-page" component={CluePage} />
         <Route path="/content" component={ContentPage} />
