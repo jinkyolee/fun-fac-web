968b2d61d458b5608e863e5899764dcd3e14686f
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-10-07 13:02:51 +0900

    Update initCurrentTab.js

--- src/js/functions/initCurrentTab.js
+++ src/js/functions/initCurrentTab.js
@@ -2,15 +2,15 @@ const initCurrentTab = () => {
   const location = window.location.pathname;
   if (location === "/") {
     return 0;
-  } else if (location === "/team-intro") {
+  } else if (location === "./team-intro") {
     return 1;
-  } else if (location === "/open-forum") {
+  } else if (location === "./open-forum") {
     return 2;
-  } else if (location === "/event") {
+  } else if (location === "./event") {
     return 3;
-  } else if (location === "/content") {
+  } else if (location === "./content") {
     return 4;
-  } else if (location === "/clue-page") {
+  } else if (location === "./clue-page") {
     return 5;
   }
 };
